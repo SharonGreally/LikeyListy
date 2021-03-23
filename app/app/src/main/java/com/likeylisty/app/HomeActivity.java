0f8435400ea9f860b59b187e5aeaedc65416e662
SharonGreally <sharon.greally@mycit.ie>
2021-03-23 13:32:45 +0000

    feat: If applied, this commit will add a delete menu item bug: If applied, this commit will fix status update bug issue #16
    - Add delete item functionality
    - pop-up menu added to recyclerview
    - edit and delete are menu options
    - delete option has been coded; edit option not yet coded
    - allow user to delete an item from list
    
       - Resolves Issue #16
    - Add delete-menu-item functionality
    
    On branch add-delete-menu-item
    Changes to be committed:
    	modified:   app/app/src/androidTest/java/com/likeylisty/app/ExampleInstrumentedTest.java
    	modified:   app/app/src/main/java/com/likeylisty/app/HomeActivity.java
    	modified:   app/app/src/main/java/com/likeylisty/app/MainActivity.java
    	modified:   app/app/src/main/java/com/likeylisty/app/database/DatabaseDetails.java
    	modified:   app/app/src/main/java/com/likeylisty/app/database/DatabaseHelper.java
    	modified:   app/app/src/main/java/com/likeylisty/app/item/Item.java
    	modified:   app/app/src/main/java/com/likeylisty/app/item/ItemAdapter.java
    	new file:   app/app/src/main/java/com/likeylisty/app/refreshAdapter.java
    	modified:   app/app/src/main/res/anim/wave_dark.xml
    	modified:   app/app/src/main/res/drawable-v24/ic_launcher_foreground.xml
    	new file:   app/app/src/main/res/drawable/ic_baseline_menu_icon.xml
    	modified:   app/app/src/main/res/drawable/ic_launcher_background.xml
    	modified:   app/app/src/main/res/drawable/likey_listy_logo.xml
    	modified:   app/app/src/main/res/drawable/splash_background.xml
    	modified:   app/app/src/main/res/drawable/splash_logo.xml
    	modified:   app/app/src/main/res/layout/activity_home.xml
    	modified:   app/app/src/main/res/layout/activity_main.xml
    	modified:   app/app/src/main/res/layout/item_layout.xml
    	new file:   app/app/src/main/res/menu/pop_menu.xml
    	modified:   app/app/src/main/res/mipmap-anydpi-v26/ic_launcher.xml
    	modified:   app/app/src/main/res/mipmap-anydpi-v26/ic_launcher_round.xml
    	modified:   app/app/src/main/res/values-night/themes.xml
    	modified:   app/app/src/main/res/values/colors.xml
    	modified:   app/app/src/main/res/values/strings.xml
    	modified:   app/app/src/main/res/values/themes.xml
    	modified:   app/app/src/test/java/com/likeylisty/app/ExampleUnitTest.java

--- app/app/src/main/java/com/likeylisty/app/HomeActivity.java
+++ app/app/src/main/java/com/likeylisty/app/HomeActivity.java
@@ -1,67 +1,68 @@
 package com.likeylisty.app;
 
 import android.app.Activity;
-import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.provider.ContactsContract;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
 import com.likeylisty.app.database.DatabaseDetails;
-import com.likeylisty.app.item.ItemAdapter;
 import com.likeylisty.app.database.DatabaseHelper;
-import com.likeylisty.app.item.Item;
+import com.likeylisty.app.item.ItemAdapter;
 
-public class HomeActivity extends Activity {
+public class HomeActivity extends Activity implements refreshAdapter {
 
-    private SQLiteDatabase db;
-    private DatabaseHelper dbHelper;
-    private EditText itemTextInput;
-    private Button itemAddButton;
-    private ItemAdapter adapter;
-    private String itemText;
+  private SQLiteDatabase db;
+  private DatabaseHelper dbHelper;
+  private EditText itemTextInput;
+  private Button itemAddButton;
+  private ItemAdapter adapter;
+  private String itemText;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_home);
 
-        dbHelper = new DatabaseHelper(this);
-        db = dbHelper.getWritableDatabase();
+    dbHelper = new DatabaseHelper(this);
+    db = dbHelper.getWritableDatabase();
 
-        RecyclerView itemListView = findViewById(R.id.item_list_view);
-        itemListView.setLayoutManager(new LinearLayoutManager(this));
-        adapter = new ItemAdapter(this, getAllItems());
-        itemListView.setAdapter(adapter);
+    RecyclerView itemListView = findViewById(R.id.item_list_view);
+    itemListView.setLayoutManager(new LinearLayoutManager(this));
+    adapter = new ItemAdapter(this, getAllItems(), this);
+    itemListView.setAdapter(adapter);
 
-        itemTextInput = findViewById(R.id.item_text_input);
-        itemAddButton = findViewById(R.id.item_add_button);
+    itemTextInput = findViewById(R.id.item_text_input);
+    itemAddButton = findViewById(R.id.item_add_button);
 
-        itemAddButton.setOnClickListener(v -> {
-            itemText = itemTextInput.getText().toString();
-            dbHelper.createItem(itemText);
-            System.out.println("---------- " + itemText);
-            adapter.swapCursor(getAllItems());
+    itemAddButton.setOnClickListener(v -> {
+      itemText = itemTextInput.getText().toString();
+      dbHelper.createItem(itemText);
+      System.out.println("---------- " + itemText);
+      refreshAdapter();
 
-            itemTextInput.getText().clear();
-        });
-    }
+      itemTextInput.getText().clear();
+    });
 
-    public Cursor getAllItems() {
-        return db.query(
-                DatabaseDetails.Item.TABLE_NAME,
-                null,
-                null,
-                null,
-                null,
-                null,
-                DatabaseDetails.Item.COLUMN_ITEM_TIMESTAMP + " DESC"
-        );
-    }
+  }
+
+  public Cursor getAllItems() {
+    return db.query(
+        DatabaseDetails.Item.TABLE_NAME,
+        null,
+        null,
+        null,
+        null,
+        null,
+        DatabaseDetails.Item.COLUMN_ITEM_TIMESTAMP + " DESC"
+    );
+  }
+
+  @Override
+  public void refreshAdapter() {
+    adapter.swapCursor(getAllItems());
+
+  }
 }
