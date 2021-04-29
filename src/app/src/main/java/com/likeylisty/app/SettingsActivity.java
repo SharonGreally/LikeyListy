6a99c6b13ffe6103d6b65a068f435868543894cb
veracit <50073051+veracit@users.noreply.github.com>
2021-04-29 14:25:35 +0100

    Enable action bar, Add Settings option, add "change background option" and back option.

---
+++ src/app/src/main/java/com/likeylisty/app/SettingsActivity.java
@@ -0,0 +1,39 @@
+package com.likeylisty.app;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.widget.TextView;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+
+        TextView button_text = (TextView) findViewById(R.id.back_view);
+
+        button_text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(SettingsActivity.this, HomeActivity.class));
+            }
+        });
+
+
+
+
+
+    }}
